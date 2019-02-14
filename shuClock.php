f795669c0ee246f4db97be8f22708e034574503a
Ameenashoukrah <47629964+Ameenashoukrah@users.noreply.github.com>
2019-02-14 07:41:11 -0800

    Add files via upload

---
+++ shuClock.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Analog Clock</title>
+    <style>
+        .clockdate-wrapper {
+            background-color: #333;
+            padding:25px;
+            max-width:350px;
+            width:100%;
+            text-align:center;
+            border-radius:5px;
+            margin:0 auto;
+            margin-top:15%;
+        }
+        #clock{
+            background-color:#333;
+            font-family: sans-serif;
+            font-size:60px;
+            text-shadow:0px 0px 1px #fff;
+            color:#fff;
+        }
+        #clock span {
+            color:#888;
+            text-shadow:0px 0px 1px #333;
+            font-size:30px;
+            position:relative;
+            top:-27px;
+            left:-10px;
+        }
+        #date {
+            letter-spacing:10px;
+            font-size:14px;
+            font-family:arial,sans-serif;
+            color:#fff;
+        }
+
+    </style>
+</head>
+
+<body onload="startTime()">
+<div style="color: white;z-index: 100;position: absolute;margin-left: 45%;margin-top: 14%;font-size: 20px;"><?php
+    echo date('d m y');
+    ?></div>
+<div id="clockdate">
+    <div class="clockdate-wrapper">
+        <div id="clock"></div>
+        <div id="date"></div>
+    </div>
+</div>
+
+
+<script>
+    function startTime() {
+        var today = new Date();
+        var hr = today.getHours();
+        var min = today.getMinutes();
+        var sec = today.getSeconds();
+        ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+        hr = (hr == 0) ? 12 : hr;
+        hr = (hr > 12) ? hr - 12 : hr;
+        //Add a zero in front of numbers<10
+        hr = checkTime(hr);
+        min = checkTime(min);
+        sec = checkTime(sec);
+        document.getElementById("clock").innerHTML = hr + " : " + min + " : " + sec + " " + ap;
+        var time = setTimeout(function(){ startTime() }, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+</script>
+</body>
+</html>
